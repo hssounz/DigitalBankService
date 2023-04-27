bf0549c62714938a15d29a89ad1502fe86a3e756
Hassen <hassenbensaad55@gmail.com>
2023-04-27 16:48:47 +0200

    Initial commit

---
+++ src/main/java/com/example/DigitalBankService/DigitalBankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.DigitalBankService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankServiceApplication.class, args);
+	}
+
+}
